993755f412db452fd544fc5c5a3ade57c9765888
helmy <helmyfadlail.5@gmail.com>
2023-03-16 17:45:31 +0700

    third commit

---
+++ dailyTaskThree.js
@@ -0,0 +1,470 @@
+let persons = [
+  {
+    _id: "6412b42737f3414721e730ea",
+    age: 30,
+    eyeColor: "green",
+    name: "Kelli Atkins",
+    address: "Bandung",
+    registered: "2020-10-26T08:35:43 -07:00",
+    friends: [
+      {
+        id: 0,
+        name: "Shifly",
+        isActive: false,
+        favoriteFruit: "strawberry",
+        gender: "female",
+        hobby: [
+          {
+            id: 2,
+            hobby: "football",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Theresia",
+        isActive: true,
+        favoriteFruit: "apple",
+        gender: "female",
+        hobby: [
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+          {
+            id: 2,
+            hobby: "football",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Shifly",
+        isActive: false,
+        favoriteFruit: "banana",
+        gender: "female",
+        hobby: [
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "6412b4271cc53ddf87a60037",
+    age: 39,
+    eyeColor: "blue",
+    name: "Black Ward",
+    address: "Jakarta",
+    registered: "2021-04-06T05:49:14 -07:00",
+    friends: [
+      {
+        id: 0,
+        name: "Shifly",
+        isActive: true,
+        favoriteFruit: "strawberry",
+        gender: "male",
+        hobby: [
+          {
+            id: 2,
+            hobby: "football",
+          },
+          {
+            id: 2,
+            hobby: "basketball",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Shifly",
+        isActive: true,
+        favoriteFruit: "apple",
+        gender: "male",
+        hobby: [
+          {
+            id: 2,
+            hobby: "football",
+          },
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Shifly",
+        isActive: true,
+        favoriteFruit: "banana",
+        gender: "male",
+        hobby: [
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "6412b42789ba9e9322e8e745",
+    age: 23,
+    eyeColor: "green",
+    name: "Stafford Osborne",
+    address: "Jakarta",
+    registered: "2020-05-10T09:40:33 -07:00",
+    friends: [
+      {
+        id: 0,
+        name: "Farid",
+        isActive: false,
+        favoriteFruit: "apple",
+        gender: "male",
+        hobby: [
+          {
+            id: 2,
+            hobby: "football",
+          },
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Shifly",
+        isActive: false,
+        favoriteFruit: "apple",
+        gender: "male",
+        hobby: [
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Theresia",
+        isActive: true,
+        favoriteFruit: "apple",
+        gender: "male",
+        hobby: [
+          {
+            id: 1,
+            hobby: "football",
+          },
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "6412b427173630e3a4aac11b",
+    age: 36,
+    eyeColor: "brown",
+    name: "Tabitha Taylor",
+    address: "Bandung",
+    registered: "2022-03-07T02:03:44 -07:00",
+    friends: [
+      {
+        id: 0,
+        name: "Shifly",
+        isActive: false,
+        favoriteFruit: "banana",
+        gender: "female",
+        hobby: [
+          {
+            id: 2,
+            hobby: "basketball",
+          },
+          {
+            id: 2,
+            hobby: "football",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Theresia",
+        isActive: false,
+        favoriteFruit: "banana",
+        gender: "female",
+        hobby: [
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Theresia",
+        isActive: true,
+        favoriteFruit: "banana",
+        gender: "female",
+        hobby: [
+          {
+            id: 1,
+            hobby: "football",
+          },
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "6412b427d6a45691fe429a57",
+    age: 23,
+    eyeColor: "blue",
+    name: "Harriet Perry",
+    address: "Bandung",
+    registered: "2022-10-09T02:33:45 -07:00",
+    friends: [
+      {
+        id: 0,
+        name: "Farid",
+        isActive: false,
+        favoriteFruit: "banana",
+        gender: "female",
+        hobby: [
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Farid",
+        isActive: false,
+        favoriteFruit: "banana",
+        gender: "female",
+        hobby: [
+          {
+            id: 2,
+            hobby: "basketball",
+          },
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Theresia",
+        isActive: true,
+        favoriteFruit: "strawberry",
+        gender: "female",
+        hobby: [
+          {
+            id: 2,
+            hobby: "football",
+          },
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "6412b4277f49c57cf99c25d1",
+    age: 26,
+    eyeColor: "blue",
+    name: "Schwartz Velez",
+    address: "Jakarta",
+    registered: "2021-09-01T10:45:42 -07:00",
+    friends: [
+      {
+        id: 0,
+        name: "Shifly",
+        isActive: false,
+        favoriteFruit: "strawberry",
+        gender: "male",
+        hobby: [
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Farid",
+        isActive: false,
+        favoriteFruit: "banana",
+        gender: "male",
+        hobby: [
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+          {
+            id: 2,
+            hobby: "football",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Shifly",
+        isActive: true,
+        favoriteFruit: "apple",
+        gender: "male",
+        hobby: [
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "6412b4276c88fb47fdf28bc6",
+    age: 39,
+    eyeColor: "green",
+    name: "Margery Vincent",
+    address: "Bandung",
+    registered: "2020-07-14T10:35:08 -07:00",
+    friends: [
+      {
+        id: 0,
+        name: "Theresia",
+        isActive: true,
+        favoriteFruit: "apple",
+        gender: "female",
+        hobby: [
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Farid",
+        isActive: true,
+        favoriteFruit: "banana",
+        gender: "female",
+        hobby: [
+          {
+            id: 1,
+            hobby: "baseball",
+          },
+          {
+            id: 1,
+            hobby: "football",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Farid",
+        isActive: true,
+        favoriteFruit: "strawberry",
+        gender: "female",
+        hobby: [
+          {
+            id: 2,
+            hobby: "baseball",
+          },
+          {
+            id: 1,
+            hobby: "basketball",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+function checkValue(key, value) {
+  return key === value;
+}
+
+function display(data, number) {
+  let arr = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].friends.length; j++) {
+      switch (number) {
+        case 6:
+          if (checkValue(true, data[i].friends[j].isActive) && checkValue("female", data[i].friends[j].gender) && checkValue("strawberry", data[i].friends[j].favoriteFruit)) {
+            arr.push(data[i].friends[j].name);
+          }
+          break;
+        case 7:
+          if (checkValue("Theresia", data[i].friends[j].name)) {
+            arr.push(data[i].name);
+          }
+          break;
+        case 9:
+          for (let k = 0; k < data[i].friends[j].hobby.length; k++) {
+            if (checkValue("brown", data[i].eyeColor) && checkValue("banana", data[i].friends[j].favoriteFruit) && checkValue("basketball", data[i].friends[j].hobby[k].hobby)) {
+              arr.push(data[i].name);
+            }
+          }
+          break;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log("6). display friends yang isActive nya true dan gender nya female dan favorite fruit nya strawberry:");
+console.log(display(persons, 6));
+
+console.log("7). display siapa saja yang mempunyai teman name Theresia:");
+console.log(display(persons, 7));
+
+console.log("9). display person yang eye color nya brown dan favorite fruit nya banana dan mempunyai teman hobby basketball:");
+console.log(display(persons, 9));
